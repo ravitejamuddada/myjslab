beb964cd157c6f51fdc5bf9551fb52618e81f7ea
Ravi Teja Muddada <33419617+myjscoffee@users.noreply.github.com>
2020-08-17 17:11:02 +0530

    Create non overlapping intervals.js

---
+++ non overlapping intervals.js
@@ -0,0 +1,34 @@
+Example 1:
+
+Input: [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
+Example 2:
+
+Input: [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
+Example 3:
+
+Input: [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ 
+ 
+ 
+ ---------------------
+ /**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+	// sort by earliest finish time
+    intervals.sort((a, b) => a[1] - b[1]);
+    let prev = intervals[0], remove = 0;
+    
+    for(let i = 1; i < intervals.length; i++) {
+        if(intervals[i][0] < prev[1]) remove++;
+        else prev = intervals[i];
+    }
+    return remove;
+};
