ad8ae933ccb1693d1904b9e46a49ce0c104c6fa9
Ravi Teja Muddada <33419617+myjscoffee@users.noreply.github.com>
2020-09-01 19:58:05 +0530

    Update reverse vowels in a string.js

--- reverse vowels in a string.js
+++ reverse vowels in a string.js
@@ -17,3 +17,29 @@ var reverseVowels = function(s) {
     } ).forEach((v,i,res)=>{arr[vowelIndices[i]]=res[res.length-i-1]})
     return arr.join('');
 };
+
+
+
+--------------------------------------2----------------------------------
+
+
+var reverseVowels = function(s) {
+  if (s.length <= 1) { return s; }
+  const str = [...s];
+  let left = 0;
+  let right = s.length - 1;
+  const set = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+
+  while(left < right) {
+    if (set.has(str[left]) && set.has(str[right])) {
+      [str[right], str[left]] = [str[left], str[right]];      
+      left++;
+      right--;
+    } else if (set.has(str[left])) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return str.join('');
+};
